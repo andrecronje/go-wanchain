812ca29f0e33acc4316d8824853fe75ba40769a2
Andre Cronje <andre@mfin.io>
2018-05-10 18:48:09 +0200

    Imported old data structure
    Signed-off-by: Andre Cronje <andre@mfin.io>

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -55,6 +55,23 @@ type Transaction struct {
 	from atomic.Value
 }
 
+type oldtxdata struct {
+	AccountNonce uint64          `json:"nonce"    gencodec:"required"`
+	Price        *big.Int        `json:"gasPrice" gencodec:"required"`
+	GasLimit     *big.Int        `json:"gas"      gencodec:"required"`
+	Recipient    *common.Address `json:"to"       rlp:"nil"` // nil means contract creation
+	Amount       *big.Int        `json:"value"    gencodec:"required"`
+	Payload      []byte          `json:"input"    gencodec:"required"`
+
+	// Signature values
+	V *big.Int `json:"v" gencodec:"required"`
+	R *big.Int `json:"r" gencodec:"required"`
+	S *big.Int `json:"s" gencodec:"required"`
+
+	// This is only used when marshaling to JSON.
+	Hash *common.Hash `json:"hash" rlp:"-"`
+}
+
 type txdata struct {
 	Txtype uint64 `json:"Txtype"    gencodec:"required"`
 
