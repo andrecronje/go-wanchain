8b94a0085df3bd0e5b80e99f87ce41fb9d2efa1f
Andre Cronje <andre@mfin.io>
2018-05-11 02:03:03 +0200

    No clue
    Signed-off-by: Andre Cronje <andre@mfin.io>

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -202,7 +202,7 @@ func (tx *Transaction) EncodeRLP(w io.Writer) error {
 // DecodeRLP implements rlp.Decoder
 func (tx *Transaction) DecodeRLP(s *rlp.Stream) error {
 	_, size, _ := s.Kind()
-	*a := *s
+	var *a = *s
 	err := s.Decode(&tx.data)
 	if err == nil {
 		tx.size.Store(common.StorageSize(rlp.ListSize(size)))
