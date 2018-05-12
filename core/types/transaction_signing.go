241cb11463bea2af2b129a2f900892b447c1a254
Andre Cronje <andre@mfin.io>
2018-05-12 02:03:03 +0200

    Only TxType hash
    Signed-off-by: Andre Cronje <andre@mfin.io>

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -160,28 +160,16 @@ func (s EIP155Signer) SignatureValues(tx *Transaction, sig []byte) (R, S, V *big
 // Hash returns the hash to be signed by the sender.
 // It does not uniquely identify the transaction.
 func (s EIP155Signer) Hash(tx *Transaction) common.Hash {
-	if (IsLegacyTransaction(tx.Txtype())) {
-		return rlpHash([]interface{}{
-			tx.data.AccountNonce,
-			tx.data.Price,
-			tx.data.GasLimit,
-			tx.data.Recipient,
-			tx.data.Amount,
-			tx.data.Payload,
-			s.chainId, uint(0), uint(0),
-		})
-	} else {
-		return rlpHash([]interface{}{
-			tx.data.Txtype,
-			tx.data.AccountNonce,
-			tx.data.Price,
-			tx.data.GasLimit,
-			tx.data.Recipient,
-			tx.data.Amount,
-			tx.data.Payload,
-			s.chainId, uint(0), uint(0),
-		})
-	}
+	return rlpHash([]interface{}{
+		tx.data.Txtype,
+		tx.data.AccountNonce,
+		tx.data.Price,
+		tx.data.GasLimit,
+		tx.data.Recipient,
+		tx.data.Amount,
+		tx.data.Payload,
+		s.chainId, uint(0), uint(0),
+	})
 }
 
 // HomesteadTransaction implements TransactionInterface using the
